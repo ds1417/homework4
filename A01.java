5eea1d5d0fb485b5d320e6e626e3e4774ed6f63f
jeong da seul <ds1417@naver.com>
2016-10-19 22:05:39 +0900

    01번 생성

---
+++ A01.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ import java.io.*;
+   class A01{
+              void input(){
+	                 Scanner s = new Scanner(System.in);
+			            System.out.print("Input Day:");
+				               int day = s.nextInt();
+					                  int seconds = day*24*60*60;
+							             print("day : " + day + "    "+ "seconds : "  + seconds );
+								                    }
+										                     void print(String str){
+												                      System.out.println(str);
+														                                                 }
+																				                            public static void main(String [] args) throws IOException{
+																							                                                 new A01().input();
+																													                                                               }
+																																				                                                           }
